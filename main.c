6da41c188c9aff8bcda1523f93fb2e9f0cd57b77
Teresa Chow <146003005+teresa-chow@users.noreply.github.com>
2024-07-22 17:39:36 +0100

    wip: update

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tchow-so <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 10:57:11 by tchow-so          #+#    #+#             */
-/*   Updated: 2024/07/05 10:42:54 by tchow-so         ###   ########.fr       */
+/*   Updated: 2024/07/22 17:38:52 by tchow-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,15 @@ int	main(int argc, char **argv)
 		return (write(2, "Error\n", 6));
 	else if (argc == 2)
 		argv = ft_split(argv[1], ' ');
-//TODO
+	if (stack_size(a) == 2) //create spare function sort_stack for these different scenarios ?
+		sort_two(&a);
+	else if (stack_size(a) == 3)
+		sort_three(&a);
+	else if (stack_size(a) == 4)
+		sort_four(&a, &b);
+	else if (stack_size(a) == 5)
+		sort_five(&a, &b);
+	else
+		sort_any(&a, &b);
+	//TODO
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: tchow-so <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 10:53:58 by tchow-so          #+#    #+#             */
-/*   Updated: 2024/06/06 14:36:57 by tchow-so         ###   ########.fr       */
+/*   Updated: 2024/07/22 17:32:16 by tchow-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,15 @@
 
 # include <stdlib.h>
 # include <limits.h> // INT_MIN + INT_MAX
-# include <stdbool.h> // true + false
 # include <unistd.h>
 
 # include "libft/libft/libft.h"
+
+/* typedef struct	s_stack
+{
+	int	*node_index;
+	int	len;
+}		t_stack; */
 
 typedef struct	s_stack_node
 {
@@ -26,35 +31,48 @@ typedef struct	s_stack_node
 	int		current_index;
 	int		final_index;
 	int		cost;
-	bool		above_median;
-	bool		cheapest;
+	int		above_median;
+	int		cheapest;
 	struct s_stack_node	*target_node;
 	struct s_stack_node	*next;
 	struct s_stack_node	*prev;
 }		t_stack_node;
 
-// Handle errors
+/* =============================== PARSING ================================== */
+
+
+/* =========================== ERROR HANDLING =============================== */
+
+
+/* ================================ UTILS =================================== */
 // Stack initiation
 // Node initiation
-
-// STACK UTILS
+// Stack utils
 int	stack_size(t_stack_node **stack);
 t_stack_node	*stack_last(t_stack_node **stack);
 
 
-// COMMANDS
+/* ============================== INSTRUCTIONS ============================== */
 void	sa(t_stack_node **a);
 void	sb(t_stack_node **b);
 void	ss(t_stack_node **a, t_stack_node **b);
-pa
-pb
+void	pa(t_stack_node **a, t_stack_node **b);
+void	pb(t_stack_node **a, t_stack_node **b);
 void	ra(t_stack_node **a);
 void	rb(t_stack_node **b);
 void	rr(t_stack_node **a, t_stack_node **b);
-rra
-rrb
-rrr
+void	rra(t_stack_node **a);
+void	rrb(t_stack_node **b);
+void	rrr(t_stack_node **a, t_stack_node **b); 
 
-// Algorithms
+/* ================================ SORTING ================================= */
+// Utils
+int	sort_check(t_stack_node *stack);
+// Sorting algorithms
+void	sort_two(t_stack_node **a);
+void	sort_three(t_stack_node **a);
+void	sort_four(t_stack_node **a, t_stack_node **b);
+void	sort_five(t_stack_node **a, t_stack_node **b);
+void	sort_any(t_stack_node **a, t_stack_node **b);
 
 #endif
