b4207bde30ab95d8f5f412408e59cf102d738131
Teresa Chow <teresa-chow@users.noreply.github.com>
2025-01-02 18:24:31 +0000

    sorting OK, TO DO optimize stack b mov

--- src/sort_any_calc_ops.c
+++ src/sort_any_calc_ops.c
@@ -13,22 +13,24 @@
 #include "../include/push_swap.h"
 
 static int find_next_elem(t_elem **a, t_elem *b);
+//static void calc_rotate_b (int i, int size, t_moves *moves);
 static void calc_rotate_a (t_elem **a, int target, t_moves *moves);
 static void calc_rotate_both(t_moves *moves);
 
 void    calc_ops(t_elem **a, t_elem **b, t_moves *moves)
 {
-    int size_b;
     int i;
+    //int size_b;
     int  target;
     t_elem  *current_b;
 
-    size_b = stack_size(b);
     current_b = *b;
+    //size_b = stack_size(b);
     i = 0;
-    while (current_b && (i < size_b))
+    while (current_b)
     {
         reset_moves(moves);
+        //calc_rotate_b(i, size_b, moves);
         target = find_next_elem(a, *b);
         calc_rotate_a(a, target, moves);
         calc_rotate_both(moves);
@@ -37,32 +39,20 @@ void    calc_ops(t_elem **a, t_elem **b, t_moves *moves)
     }
 }
 
-/* Find the smallest element in stack a greater than *b,
-if not possible, return stack a min element */
+/* Find the smallest element in stack a greater than *b */
 static int find_next_elem(t_elem **a, t_elem *b)
 {
-    //bool    next_higher;
     int     target;
     t_elem  *current_a;
-    //t_elem  *min;
 
-    //next_higher = 0;
     current_a = *a;
     target = INT_MAX;
     while (current_a)
     {
         if ((current_a->val > b->val) && (current_a->val < target))
-        {
             target = current_a->val;
-            //next_higher = 1;
-        }
         current_a = current_a->next;
     }
-    /*if (!next_higher) // with this algorithm, do we evere get here? top 5 already in stack a
-    {
-        min = stack_min(a);
-        target = min->val;
-    }*/
     return (target);
 }
 
@@ -96,13 +86,13 @@ static void calc_rotate_a (t_elem **a, int target, t_moves *moves)
 
 static void calc_rotate_both(t_moves *moves)
 {
-    while (moves->ra && moves->rb)
+    while ((moves->ra > 0) && (moves->rb > 0))
     {
         moves->ra--;
         moves->rb--;
         moves->rr++;
     }
-    while (moves->rra && moves->rrb)
+    while ((moves->rra > 0) && (moves->rrb > 0))
     {
         moves->rra--;
         moves->rrb--;
